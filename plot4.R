e4971b4b6114d1de2c235b03eaeb54b8f02384d8
zilben <zilben888@yahoo.com>
2017-02-12 01:10:22 -0500

    Add files via upload
    Four R code files to generate the four plots and the four png files

---
+++ plot4.R
@@ -0,0 +1,69 @@
+##Read data from .txt file
+HPCdata <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+# 1.Date: Date in format dd/mm/yyyy
+# 2.Time: time in format hh:mm:ss
+# 3.Global_active_power: household global minute-averaged active power (in kilowatt)
+# 4.Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+# 5.Voltage: minute-averaged voltage (in volt)
+# 6.Global_intensity: household global minute-averaged current intensity (in ampere)
+# 7.Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+# 8.Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+# 9.Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+head(HPCdata)
+HPCdata2 <- HPCdata
+
+#Combine Date Time Columns
+HPCdata2$Date <- as.Date(HPCdata2$Date, format="%d/%m/%Y")
+HPCdata2$Time <- paste(HPCdata2$Date, HPCdata2$Time, sep = " ") 
+
+##Filter by only specific days
+HPCdata2 <- HPCdata2[HPCdata2$Date == "2007/2/1" | HPCdata2$Date == "2007/2/2",]
+
+HPCdata3 <- HPCdata2
+
+#Format Time column
+HPCdata3$Time<- strptime(HPCdata3$Time,format="%Y-%m-%d %H:%M:%S")
+head(HPCdata3)
+
+#Plot4
+library(datasets)
+
+png(filename = "plot4.png", width = 480 , height = 480 )
+
+par(mfcol = c(2, 2))
+
+##PlotA
+with(HPCdata3,plot(Time,Global_active_power, 
+                   type = "l",
+                   xlab = "", 
+                   ylab = "Global Active Power (kilowatts)"))
+
+##PlotB
+with(HPCdata3,plot(Time,Sub_metering_1,
+                   type = "l",
+                   xlab = "", 
+                   ylab = "Energy sub metering"))
+lines(HPCdata3$Time,HPCdata3$Sub_metering_2, col = "red")
+lines(HPCdata3$Time,HPCdata3$Sub_metering_3, col = "blue")
+legend("topright",
+       lty = 1, pch = NA, 
+       bty = "n",
+       cex=1.1,
+       col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+##PlotC
+with(HPCdata3,plot(Time,Voltage, 
+                   type = "l",
+                   xlab = "", 
+                   ylab = "Global Active Power (kilowatts)"))
+##PlotD
+with(HPCdata3,plot(Time,Global_reactive_power, 
+                   type = "l",
+                   xlab = "", 
+                   ylab = "Global Active Power (kilowatts)"))
+
+dev.off() ## Don't forget to close the PNG device!
+
